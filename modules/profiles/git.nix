b981a9158c1ae30ec07792b429ee657b3c4cad44
Dominik Stodolny <dominik@stodolny.org>
2019-07-26 10:51:55 +0100

    add git configuration
    Signed-off-by: Dominik Stodolny <dominik@stodolny.org>

--- modules/profiles/git.nix
+++ modules/profiles/git.nix
@@ -15,78 +15,79 @@ in
     };
   };
   config = mkIf cfg.enable {
-  #   programs.git = {
-  #     enable = true;
-  #     userName = "Dominik Stodolny";
-  #     userEmail = "dominik@stodolny.org";
-  #     signing = {
-  #       key = "0D5591D3B4BB7818";
-  #       signByDefault = false;
-  #     };
-  #     aliases = {
-  #       b = "branch --color -v";
-  #       br = "branch";
-  #       ci = "commit --signoff";
-  #       co = "checkout";
-  #       ca = "commit --amend";
-  #       unstage = "reset HEAD";
-  #       r = "remote -v";
-  #       st = "status";
-  #       w = "status -sb";
-  #     };
-  #     extraConfig = {
-  #     core = {
-  #       editor = "${pkgs.emacs}/bin/emacsclient -t";
-  #     };
-  #     forge = {
-  #       remote = "upstream";
-  #     };
-  #     color = {
-  #       status      = "auto";
-  #       diff        = "auto";
-  #       branch      = "auto";
-  #       interactive = "auto";
-  #       ui          = "auto";
-  #       sh          = "auto";
-  #     };
-  #     "color \"branch\"" = {
-  #       current = "cyan reverse";
-  #       local = "cyan";
-  #       remote = "green";
-  #     };
-  #     "color \"diff\"" = {
-  #       current = "white reverse";
-  #       frag = "magenta reverse";
-  #       old = "red";
-  #       new = "green";
-  #     };
-  #     "color \"status\"" = {
-  #       added = "green";
-  #       changed = "yellow";
-  #       untracked = "red";
-  #     };
-  #     hub = {
-  #       protocol = true;
-  #     };
-  #     pull = {
-  #       rebase = true;
-  #     };
-  #     push = {
-  #       default = "upstream";
-  #       recurseSubmodules = "check";
-  #     };
-  #     rebase = {
-  #       autosquash = true;
-  #     };
-  #     advice = {
-  #       statusHints = false;
-  #       pushNonFastForward = false;
-  #     };
-  #     "diff \"gpg\"" = {
-  #       binary = true;
-  #       textconv = "gpg -d --quiet --yes --compress-algo=none --no-encrypt-to";
-  #     };
-  #   };
+    programs.git = {
+      enable = true;
+      userName = "Dominik Stodolny";
+      userEmail = "dominik@stodolny.org";
+      signing = {
+        key = "0D5591D3B4BB7818";
+        signByDefault = true;
+      };
+      aliases = {
+        b = "branch --color -v";
+        br = "branch";
+        ci = "commit --signoff";
+        co = "checkout";
+        ca = "commit --amend";
+        unstage = "reset HEAD";
+        r = "remote -v";
+        st = "status";
+        w = "status -sb";
+      };
+      extraConfig = {
+        core = {
+          editor = "${pkgs.emacs}/bin/emacsclient -t";
+        };
+        forge = {
+          remote = "upstream";
+        };
+        color = {
+          status      = "auto";
+          diff        = "auto";
+          branch      = "auto";
+          interactive = "auto";
+          ui          = "auto";
+          sh          = "auto";
+        };
+        "color \"branch\"" = {
+          current = "cyan reverse";
+          local = "cyan";
+          remote = "green";
+        };
+        "color \"diff\"" = {
+          current = "white reverse";
+          frag = "magenta reverse";
+          old = "red";
+          new = "green";
+        };
+        "color \"status\"" = {
+          added = "green";
+          changed = "yellow";
+          untracked = "red";
+        };
+        hub = {
+          protocol = true;
+        };
+        pull = {
+          rebase = true;
+        };
+        push = {
+          default = "upstream";
+          recurseSubmodules = "check";
+        };
+        rebase = {
+          autosquash = true;
+        };
+        advice = {
+          statusHints = false;
+          pushNonFastForward = false;
+        };
+        "diff \"gpg\"" = {
+          binary = true;
+          textconv = "gpg -d --quiet --yes --compress-algo=none --no-encrypt-to";
+        };
+      };
+    };
   };
   # ignores = [
   #   "*.o" "*.a" "*.la" "*.so"
