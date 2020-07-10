b6c9d7c905ff18718e6544068439b0ee5992fbb4
Dominik Stodolny <dominik@stodolny.org>
2020-07-10 08:42:19 +0100

    remove unused sources

--- nix/default.nix
+++ nix/default.nix
@@ -2,8 +2,5 @@ let
   sources = import ./sources.nix;
 in
 rec {
-  home-manager = import (sources.home-manager + "/nixos");
-  nixpkgs = import sources.nixpkgs;
   emacs = import sources.emacs-overlay;
-  nixos-hardware = import sources.nixos-hardware;
 }
