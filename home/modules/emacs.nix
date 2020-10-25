cdb6e79979e8eda1d247f4b66a8417c06fefa874
Dominik Stodolny <dominik@stodolny.org>
2020-10-25 12:05:59 +0000

    install elfeed ledger-mode and orderless for emacs

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -9,6 +9,7 @@
       add-node-modules-path
       diff-hl
       direnv
+      elfeed
       exec-path-from-shell
       expand-region
       flycheck
@@ -16,11 +17,13 @@
       hl-todo
       ibuffer-vc
       icomplete-vertical
+      ledger-mode
       lsp-mode
       magit
       modus-operandi-theme
       modus-vivendi-theme
       nix-mode
+      orderless
       projectile
       rainbow-mode
       rg
